76e177412d4b208d037d02432ff634ffe3ea3cc1
Bowen Sun <bowensun@gmail.com>
2017-10-17 14:42:19 +0100

    typo

--- run.sh
+++ run.sh
@@ -10,6 +10,6 @@ aws ec2 create-snapshot --volume-id $EBS_ID --description "$EBS_ID-$DATE"
 
 EXIPRED=$(aws ec2 describe-snapshots | jq -rC '.Snapshots[] | select(.VolumeId=="$ENV.EBS_ID") | select(.StartTime < "$ENV.EXPIRE_DATE").SnapshotId')
 
-FOR X in $EXIPRED; do
+for X in $EXIPRED; do
   aws ec2 delete-snapshot --snapshot-id $X
 done
